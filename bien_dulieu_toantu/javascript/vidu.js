88e5ecaa0e51f9cc648d2d7e6a6543784f2c38c9
= <=>
2023-07-07 16:02:39 +0700

    bai tap file

---
+++ bien_dulieu_toantu/javascript/vidu.js
@@ -0,0 +1,14 @@
+var month = 5;
+switch (month) {
+    case 1, 3, 5, 7, 9,11:
+        console.log('có 30 ngày ');
+        break;
+    case 2:
+        console.log('có 28 ngày ');
+        break;
+    case 4, 6, 8, 10, 12:
+        console.log('có 31 ngày ');
+            break;
+    default:
+        ('Không hợp lệ')
+}
